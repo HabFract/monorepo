829b868dae9b022c3af679d40e6bcd6b6b4adafe
Nicholas Stebbings <nickolarse@riseup.net>
2023-10-31 12:11:00 +1300

    add flowbite dependency and test on app page

---
+++ packages/app/src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { DarkThemeToggle, Flowbite, Button } from 'flowbite-react';
+import { Card } from 'flowbite-react';
+
+export default function MyPage() {
+  return (
+    <Flowbite>
+      <DarkThemeToggle />
+      <Card
+      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgSrc="/images/blog/image-1.jpg"
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <p>
+          Noteworthy technology acquisitions 2021
+        </p>
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        <p>
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </p>
+    </Card> 
+      <Button>Click me</Button>
+    </Flowbite>
+  );
+}
