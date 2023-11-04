f62794b96094ac56fde684493b9a63c9999deee8
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-04 13:02:25 +1300

    aider: Refactored ListSpheres component to use the getSpheres mutation and display the results.
    # Aider chat conversation:
    
    USER: Now change the ListSheres component to use the getSpheres mutation and list out the results
    
    ASSISTANT: To implement this change, we need to modify the `ListSpheres.tsx` file. We will use the `useQuery` hook from `@apollo/client` to fetch the spheres using the `GET_SPHERES` query. Then, we will map over the fetched spheres and display their names.
    
    Here is the *SEARCH/REPLACE block* for the change:
    
    ```tsx
    packages/app/src/components/ListSpheres.tsx
    <<<<<<< SEARCH
    import React from 'react';
    
    function ListSpheres() {
      return (
        <div>
          <h2>List Spheres</h2>
          {/* Add your list or other UI elements here */}
        </div>
      );
    }
    
    export default ListSpheres;
    =======
    import React from 'react';
    import { useQuery } from '@apollo/client';
    import { GET_SPHERES } from '../graphql/queries/sphere';
    
    function ListSpheres() {
      const { loading, error, data } = useQuery(GET_SPHERES);
    
      if (loading) return <p>Loading...</p>;
      if (error) return <p>Error :(</p>;
    
      return (
        <div>
          <h2>List Spheres</h2>
          {data.spheres.map(({ name }) => (
            <p key={name}>{name}</p>
          ))}
        </div>
      );
    }
    
    export default ListSpheres;
    >>>>>>> REPLACE
    ```
    
    Please note that this change assumes that the `GET_SPHERES` query is defined in the `../graphql/queries/sphere` file and it fetches an array of spheres, each having a `name` property. If the query or the data structure is different, the code needs to be adjusted accordingly.

--- packages/app/src/components/ListSpheres.tsx
+++ packages/app/src/components/ListSpheres.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_SPHERES } from '../graphql/queries/sphere';
 
 function ListSpheres() {
+  const { loading, error, data } = useQuery(GET_SPHERES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div>
       <h2>List Spheres</h2>
-      {/* Add your list or other UI elements here */}
+      {data.spheres.map(({ name }) => (
+        <p key={name}>{name}</p>
+      ))}
     </div>
   );
 }
