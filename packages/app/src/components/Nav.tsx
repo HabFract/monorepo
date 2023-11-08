75052f704d10b89a429ac1130bdc23e8f80e725d
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-08 17:28:20 +1300

    Add relevant nav item buttons

--- packages/app/src/components/Nav.tsx
+++ packages/app/src/components/Nav.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@apollo/client";
 import GET_SPHERES from '../graphql/queries/sphere/getSpheres.graphql';
 
-import { MailOutlined, AppstoreOutlined } from "@ant-design/icons";
+import { MenuUnfoldOutlined, MenuFoldOutlined, PlusCircleOutlined, DashboardFilled, UnorderedListOutlined, PieChartFilled } from "@ant-design/icons";
 import Menu, { MenuProps } from "antd/es/menu/menu";
-import { Sphere, SphereConnection, SphereEdge } from "../graphql/mocks/generated";
+import { SphereConnection, SphereEdge } from "../graphql/mocks/generated";
+import { useState } from "react";
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -24,21 +25,33 @@ function getItem(
 }
 
 function createFixedMenuItems() {
-  return [getItem('S1', '2', <MailOutlined />),getItem('S1', '3', <MailOutlined />)]  
+  return [
+    getItem('List Spheres', 'list-spheres', <UnorderedListOutlined />),
+    getItem('Dashboard', 'dash', <DashboardFilled />),
+    getItem('Visualise', 'vis', <PieChartFilled />),
+  ]  
 }
 
 function createSphereMenuItems({ spheres }: { spheres: SphereConnection }) {
   console.log('spheres :>> ', spheres);
-  return spheres.edges!.map((sphere: SphereEdge) => {
-    return getItem('S1', sphere.node.id, <MailOutlined />, [
-    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
-    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
-  ])}) 
+  return [...spheres.edges!.map((sphere: SphereEdge, idx: number) => {
+    return getItem(`S${(idx + 1).toString()}`, sphere.node.id, null,
+      [
+        getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '1')], 'group'),
+        getItem('Item 2', 'g2', null, [getItem('Option 3', '1'), getItem('Option 4', '1')], 'group'),
+      ])
+  }),
+  getItem('Add Sphere', 'add-sphere', <PlusCircleOutlined />)] 
 }
 
 
 const Nav: React.FC = ({children, transition} : any) => {
   const { loading, error, data: spheres } = useQuery(GET_SPHERES);
+
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
 
   const onClick: MenuProps['onClick'] = (e) => {
     switch (e.key) {
@@ -58,17 +71,30 @@ const Nav: React.FC = ({children, transition} : any) => {
 
   return (
     <>
-      <nav className={"fixed inset-y-0 left-0 z-10"}>
+      <nav className={"fixed inset-y-0 left-0 z-10 h-full flex justify-between flex-col"}>
         {error && "Error"}
         {loading ? "Loading" :
           <Menu
+            inlineCollapsed={collapsed}
             onClick={onClick}
-            style={{ width: 256 }}
+            style={{ width: collapsed ? 72 : 256 }}
             defaultSelectedKeys={['1']}
             defaultOpenKeys={['sub1']}
             mode="inline"
-            items={[...createSphereMenuItems(spheres), ...createFixedMenuItems()]}
+            items={createSphereMenuItems(spheres)}
           />}
+          <div className={"flex flex-col items-center mb-4 gap-2"}>
+            <Menu
+              inlineCollapsed={collapsed}
+              onClick={onClick}
+              style={{ width: 72 }}
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              mode="inline"
+              items={createFixedMenuItems()}
+            />
+            {collapsed ? <MenuUnfoldOutlined onClick={toggleCollapsed}/> : <MenuFoldOutlined onClick={toggleCollapsed}/>}
+          </div>
       </nav>
       <main className={"fixed w-full h-full inset-y-0 left-0"}>{children}</main>
     </>
