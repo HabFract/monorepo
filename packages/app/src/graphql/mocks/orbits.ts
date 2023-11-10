c61eacb31aac87a0ac01a4de3829421702de6839
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-10 16:43:57 +1300

    aider: Mock the new query `getOrbitsBySphere` in `packages/app/src/graphql/mocks/orbits.ts` to return filtered mocks based on the input sphere id.
    # Aider chat conversation:
    
    USER: Now edit my `packages/app/src/graphql/mocks/orbits.ts` so that this new query is mocked, and that if the input is a certain sphere id, it will return the filtered mocks.
    
    ASSISTANT: To mock the new query `getOrbitsBySphere` in the `packages/app/src/graphql/mocks/orbits.ts` file, we will need to add a new mock entry for the query with the specific `sphereEntryHashB64` variable. This mock will return only the orbits that match the provided `sphereEntryHashB64`.
    
    Here's how you can update the `orbits.ts` file:
    
    ```typescript
    packages/app/src/graphql/mocks/orbits.ts
    <<<<<<< SEARCH
    export const ORBITS_MOCKS = [{
    =======
    import GET_ORBITS_BY_SPHERE from '../queries/orbit/getOrbitsBySphere.graphql';
    
    export const ORBITS_MOCKS = [
      {
        request: {
          query: GET_ORBITS_BY_SPHERE,
          variables: { sphereEntryHashB64: 'SGVhbHRoMQ==' }, // Base64 for "Health and Fitness" sphere id
        },
        result: {
          data: {
            orbits: aOrbitConnection({
              edges: [
                {
                  node: {
                    id: 'R28gZm9yIGEgd2Fsay==', // Base64 for "Go for a walk"
                    name: 'Go for a walk',
                    metadata: {
                      description: 'A daily walk to improve cardiovascular health.',
                      frequency: Frequency.DAY,
                      scale: Scale.ATOM,
                    },
                    timeframe: {
                      startTime: 1617235200, // Mocked Unix timestamp for example
                      endTime: 1617321600,   // Mocked Unix timestamp for example
                    },
                    sphereEntryHashB64: 'SGVhbHRoMQ==', // Corresponding to "Health and Fitness" sphere id
                  },
                  cursor: ''
                },
                {
                  node: {
                    id: 'TGlmdCB3ZWlnaHRz', // Base64 for "Lift weights"
                    name: 'Lift weights',
                    metadata: {
                      description: 'Strength training to build muscle and increase metabolism.',
                      frequency: Frequency.WEEK,
                      scale: Scale.ATOM,
                    },
                    timeframe: {
                      startTime: 1617235200, // Mocked Unix timestamp for example
                      endTime: 1617321600,   // Mocked Unix timestamp for example
                    },
                    sphereEntryHashB64: 'SGVhbHRoMQ==', // Corresponding to "Health and Fitness" sphere id
                  },
                  cursor: ''
                },
                // ... other orbits with the same sphereEntryHashB64 if any
              ],
            }),
          },
        },
      },
      // ... existing mocks
    >>>>>>> REPLACE
    ];
    ```
    
    This mock will intercept the query `getOrbitsBySphere` when the variable `sphereEntryHashB64` is equal to the Base64 encoded string for "Health and Fitness" sphere id. It will return the filtered list of orbits that belong to that sphere. If you have other sphere IDs for which you want to mock the data, you can add additional entries with the corresponding `sphereEntryHashB64` and the filtered mock data.

--- packages/app/src/graphql/mocks/orbits.ts
+++ packages/app/src/graphql/mocks/orbits.ts
@@ -4,7 +4,59 @@ import CREATE_ORBIT from '../mutations/orbit/createOrbit.graphql';
 import GET_ORBITS from '../queries/orbit/getOrbits.graphql';
 import { Frequency, Scale } from './generated';
 
-export const ORBITS_MOCKS = [{
+import GET_ORBITS_BY_SPHERE from '../queries/orbit/getOrbitsBySphere.graphql';
+
+export const ORBITS_MOCKS = [
+  {
+    request: {
+      query: GET_ORBITS_BY_SPHERE,
+      variables: { sphereEntryHashB64: 'SGVhbHRoMQ==' }, // Base64 for "Health and Fitness" sphere id
+    },
+    result: {
+      data: {
+        orbits: aOrbitConnection({
+          edges: [
+            {
+              node: {
+                id: 'R28gZm9yIGEgd2Fsay==', // Base64 for "Go for a walk"
+                name: 'Go for a walk',
+                metadata: {
+                  description: 'A daily walk to improve cardiovascular health.',
+                  frequency: Frequency.DAY,
+                  scale: Scale.ATOM,
+                },
+                timeframe: {
+                  startTime: 1617235200, // Mocked Unix timestamp for example
+                  endTime: 1617321600,   // Mocked Unix timestamp for example
+                },
+                sphereEntryHashB64: 'SGVhbHRoMQ==', // Corresponding to "Health and Fitness" sphere id
+              },
+              cursor: ''
+            },
+            {
+              node: {
+                id: 'TGlmdCB3ZWlnaHRz', // Base64 for "Lift weights"
+                name: 'Lift weights',
+                metadata: {
+                  description: 'Strength training to build muscle and increase metabolism.',
+                  frequency: Frequency.WEEK,
+                  scale: Scale.ATOM,
+                },
+                timeframe: {
+                  startTime: 1617235200, // Mocked Unix timestamp for example
+                  endTime: 1617321600,   // Mocked Unix timestamp for example
+                },
+                sphereEntryHashB64: 'SGVhbHRoMQ==', // Corresponding to "Health and Fitness" sphere id
+              },
+              cursor: ''
+            },
+            // ... other orbits with the same sphereEntryHashB64 if any
+          ],
+        }),
+      },
+    },
+  },
+  // ... existing mocks
   request: {
     query: GET_ORBITS,
     variables: {},
