2f661986660cf2401ae62967d36c1de16c91c85c
Nicholas Stebbings <nickolarse@riseup.net>
2023-12-12 10:39:22 +1300

    add profile context, form component

---
+++ packages/app/src/contexts/myProfile.tsx
@@ -0,0 +1,13 @@
+import { Profile } from '../graphql/mocks/generated';
+import { useState, useMemo, createContext } from 'react'
+
+export const MyProfileContext = createContext(null)
+
+export const MyProfileProvider = (props: any) => {
+  const [profile, setProfile] = useState<Omit<Profile, '__typename'>>(
+    props?.value,
+  )
+  const value = useMemo(() => [profile, setProfile], [profile])
+
+  return (<MyProfileContext.Provider {...props} value={value} />)
+}
---
+++ packages/app/src/hooks/useMyProfile.ts
@@ -0,0 +1,10 @@
+import { MyProfileContext } from '../contexts/myProfile'
+import { useContext } from 'react'
+
+export const useMyProfile = () => {
+  const context = useContext(MyProfileContext)
+  if (!context) {
+    throw new Error(`useMyProfile must be used within a MyProfileProvider`)
+  }
+  return context as any
+}
