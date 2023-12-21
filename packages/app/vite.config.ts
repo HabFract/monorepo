a3e1ddc252ba9720da68af02320c5befd29ce461
Nicholas Stebbings <nickolarse@riseup.net>
2023-12-22 10:16:10 +1300

    fix UI port

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import graphql from '@rollup/plugin-graphql';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    nodePolyfills(),
-    graphql()
-  ],
-  define: {
-    "process.env": {},
-  }
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+      plugins: [react(),
+        nodePolyfills(),
+        graphql()
+      ],
+      define: {
+        "process.env": {},
+      },
+      server: {
+          port: parseInt(process.env.VITE_UI_PORT),
+      },
+    })
+}
