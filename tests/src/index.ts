a3e8bc2b57b93c36f68ed892efc15df4f5c15197
Nicholas Stebbings <nickolarse@riseup.net>
2024-01-10 00:21:47 +1300

    scaffold sphere crud tests

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,6 +1,9 @@
 import {orbits1, orbits2, crud, levels} from './habits/personal/orbits';
+import {crud as crud2} from './habits/personal/sphere';
 
-// orbits1();
-// orbits2();
-// crud();
+orbits1();
+orbits2();
+crud();
 levels();
+
+crud2();
