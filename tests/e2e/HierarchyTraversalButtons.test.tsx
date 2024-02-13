db23974489a679cb49b1376424e2d7e5a31e5a31
Nicholas Stebbings <nickolarse@riseup.net>
2024-02-14 11:41:46 +1300

    update two child traversal tests

--- tests/e2e/HierarchyTraversalButtons.test.tsx
+++ tests/e2e/HierarchyTraversalButtons.test.tsx
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 import { MockedProvider } from '@apollo/client/testing';
 import OrbitTree from '../../app/src/components/vis/OrbitTree';
 import { renderVis } from '../../app/src/components/vis/helpers';
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { HIERARCHY_ROOT_ONE_CHILD_MOCKS } from './mocks/hierarchy-root-1-child';
 import { HIERARCHY_ROOT_TWO_CHILDREN_MOCKS } from './mocks/hierarchy-root-2-children';
 import { TestProvider } from '../utils-frontend';
@@ -14,8 +14,8 @@ import { SPHERE_ID } from './mocks/spheres';
 
 const Tree = renderVis(OrbitTree);
 
-describe('Hierarchy Path Templates - renders path for parent with 1 child', () => {
-  it('renders traversal button for going down a level', async () => {
+describe.skip('Hierarchy Path Templates - renders traversal buttons for parent with 1 child', () => {
+  it.skip('renders traversal button for going down a level', async () => {
     const { getByTestId, queryByTestId } = render(
       <MockedProvider mocks={HIERARCHY_ROOT_ONE_CHILD_MOCKS} addTypename={false}>
         <TestProvider initialValues={[
@@ -32,5 +32,48 @@ describe('Hierarchy Path Templates - renders path for parent with 1 child', () =
     expect(queryByTestId('traversal-button-left')).not.toBeInTheDocument();
     expect(queryByTestId('traversal-button-right')).not.toBeInTheDocument();
     expect(queryByTestId('traversal-button-up')).not.toBeInTheDocument();
+  });
+
+});
+
+describe('Hierarchy Path Templates - renders traversal buttons for parent with 2 children', () => {
+  it.skip('renders traversal button for going down a level', async () => {
+    const { getByTestId, queryByTestId } = render(
+      <MockedProvider mocks={HIERARCHY_ROOT_TWO_CHILDREN_MOCKS} addTypename={false}>
+        <TestProvider initialValues={[
+          [currentSphere, { entryHash: SPHERE_ID, actionHash: SPHERE_ID }],
+        ]}>
+          {(Tree)}
+        </TestProvider>
+      </MockedProvider>
+    );
+
+    await waitFor(() => {
+      expect(getByTestId('traversal-button-down')).toBeInTheDocument();
+    });
+    expect(queryByTestId('traversal-button-left')).not.toBeInTheDocument();
+    expect(queryByTestId('traversal-button-right')).not.toBeInTheDocument();
+    expect(queryByTestId('traversal-button-up')).not.toBeInTheDocument();
+  });
+
+  it('renders traversal button for going right', async () => {
+    const { getByTestId } = render(
+      <MockedProvider mocks={HIERARCHY_ROOT_TWO_CHILDREN_MOCKS} addTypename={false}>
+        <TestProvider initialValues={[
+          [currentSphere, { entryHash: SPHERE_ID, actionHash: SPHERE_ID }],
+        ]}>
+          {(Tree)}
+        </TestProvider>
+      </MockedProvider>
+    );
+
+    await waitFor(() => {
+      const traversalButton = getByTestId('traversal-button-down');
+      fireEvent.click(traversalButton);
+    });
+
+    await waitFor(() => {
+      expect(getByTestId('traversal-button-right')).toBeInTheDocument();
+    });
   });
 });
