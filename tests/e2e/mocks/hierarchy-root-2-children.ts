835042680c009a2b4540968412dfcb40ee9e3fc1
Nicholas Stebbings <nickolarse@riseup.net>
2024-02-14 11:55:06 +1300

    clean up mocks

--- tests/e2e/mocks/hierarchy-root-2-children.ts
+++ tests/e2e/mocks/hierarchy-root-2-children.ts
@@ -18,7 +18,6 @@ export const HIERARCHY_ROOT_TWO_CHILDREN_MOCKS = [
       data: {
         getOrbitHierarchy: `{"result": {"content":"uhCEkK4tYe6wTVt56vtr5pszKBHwjwh2cPYFv4ej5KvfX6l-ncJWu","name":"Be the best","children":[{"content":"R28gZm9yIGEgd2Fsay==1","name":"Child Node 1","children":[]},
         {"content":"R28gZm9yIGEgd2Fsay==2","name":"Child Node 2","children":[]}] }}`,
-        // getOrbitHierarchy: `{"result": {"content":"L1R20-","name":"Be the best","children":[{"content":"L2R13-","name":"Be in peak physical condition","children":[{"content":"L3R12-","name":"Have a good exercise routine","children":[{"content":"L4R5-","name":"go for a short walk at least once a day","children":[]},{"content":"L6R11-","name":"Do some weight training","children":[{"content":"L7R8-","name":"3 sets of weights, til failure","children":[]},{"content":"L9R10-","name":"Do 3 sets of calisthenic exercises","children":[]}]}]}]},{"content":"L14R19-","name":"Establish productive work habits","children":[{"content":"L15R16-","name":"Do one 50 minute pomodoro ","children":[]},{"content":"L17R18-","name":"Read more books on computing","children":[]}]}]}}`,
       },
     },
   },
