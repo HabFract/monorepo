11e882cb95cda3065949ca5145eef3b0a3afa1e9
Nicholas Stebbings <nickolarse@riseup.net>
2024-03-06 09:44:22 +1300

    update aider's messed up mocks

---
+++ tests/e2e/mocks/hierarchy-root-2-children-2-grandchildren-unbalanced.ts
@@ -0,0 +1,111 @@
+import {
+  GetOrbitHierarchyDocument,
+} from "../../../app/src/graphql/generated/index";
+import { ORBITS_MOCKS } from "./orbits";
+import { SPHERE_ID } from "./spheres";
+
+const mockOrbits = ORBITS_MOCKS[0].result.data.orbits!.edges;
+
+export const HIERARCHY_ROOT_THREE_LEVELS_UNBALANCED_MOCKS = [
+  // {
+  //   request: {
+  //     query: GetOrbitHierarchyDocument,
+  //     variables: {
+  //       params: {
+  //         orbitEntryHashB64:"uhCEkK4tYe6wTVt56vtr5pszKBHwjwh2cPYFv4ej5KvfX6l-ncJWu"
+  //       },
+  //     },
+  //   },
+  //   result: {
+  //     data: {
+  //       getOrbitHierarchy: `{"result": {"content":"R28gZm9yIGEgd2Fsay==","name":"Be the best","children":[{"content":"R28gZm9yIGEgd2Fsay==1","name":"Child Node 1 of 2","children":[]},
+  //       {"content":"R28gZm9yIGEgd2Fsay==2","name":"Child Node 2 of 2","children":[]}] }}`,
+  //     },
+  //   },
+  // },
+  {
+    request: {
+      query: GetOrbitHierarchyDocument,
+      variables: {
+        params: {
+          levelQuery: {
+            sphereHashB64: SPHERE_ID,
+            orbitLevel: 0,
+          },
+        }
+      },
+    },
+    result: {
+      data: {
+        getOrbitHierarchy: JSON.stringify({
+          result: {
+            content: "R28gZm9yIGEgd2Fsay==",
+            name: "Be the best",
+            children: [
+              {
+                content: "R28gZm9yIGEgd2Fsay==1",
+                name: "Child Node 1 of 2",
+                children: [
+                  {
+                    content: mockOrbits[2].node.id,
+                    name: mockOrbits[2].node.name,
+                    children: []
+                  },
+                  {
+                    content: mockOrbits[3].node.id,
+                    name: mockOrbits[3].node.name,
+                    children: []
+                  }
+                ]
+              },
+              {
+                content: "R28gZm9yIGEgd2Fsay==2",
+                name: "Child Node 2 of 2",
+                children: []
+              }
+            ]
+          }
+        })
+      }
+    },
+  },
+  {
+    request: {
+      query: GetOrbitHierarchyDocument,
+      variables: {
+        params: {
+          levelQuery: {
+            sphereHashB64: SPHERE_ID,
+            orbitLevel: 1,
+          },
+        }
+      },
+    },
+    result: {
+      data: {
+        getOrbitHierarchy: `{\"result\":{\"level_trees\":${JSON.stringify(
+          [{
+            content: "R28gZm9yIGEgd2Fsay==1",
+            name: "Child Node 1 of 2",
+            children: [
+              {
+                content: mockOrbits[2].node.id,
+                name: mockOrbits[2].node.name,
+                children: []
+              },
+              {
+                content: mockOrbits[3].node.id,
+                name: mockOrbits[3].node.name,
+                children: []
+              }
+            ]
+          },
+          {
+            content: "R28gZm9yIGEgd2Fsay==2",
+            name: "Child Node 2 of 2",
+            children: []
+          }])}] }}`,
+      },
+    },
+  },
+];
