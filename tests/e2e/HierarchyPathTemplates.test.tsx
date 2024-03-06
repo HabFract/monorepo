859aa35372ccf2cd678e5049ba249846afd49f36
Nicholas Stebbings <nickolarse@riseup.net>
2024-03-06 14:45:20 +1300

    first test passing (default, checking for only possible current paths)

--- tests/e2e/HierarchyPathTemplates.test.tsx
+++ tests/e2e/HierarchyPathTemplates.test.tsx
@@ -8,9 +8,30 @@ import { renderVis } from '../../app/src/components/vis/helpers';
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import { HIERARCHY_ROOT_ONE_CHILD_MOCKS } from './mocks/hierarchy-root-1-child';
 import { HIERARCHY_ROOT_TWO_CHILDREN_MOCKS } from './mocks/hierarchy-root-2-children';
-import { WithCurrentSphereMockedAtom } from '../utils-frontend';
+import { WithCurrentOrbitCoordsMockedAtom } from '../utils-frontend';
 
-describe.skip('Hierarchy Path Templates - after traversing down, it renders path an only child', () => {
+let x = 0;
+let y = 0;
+let maxBreadth: number = 0;
+
+// The following are rough versions of what is happening in internal state of jotai atoms.
+const incrementBreadth = jest.fn(() => { x += 1 }) as Function;
+const incrementDepth = jest.fn(() => { y -= 1 ; }) as Function;
+const decrementBreadth = jest.fn(() => { x -= 1  }) as Function;
+const decrementDepth = jest.fn(() => { y-=1  }) as Function;
+
+
+jest.mock("../../app/src/hooks/useNodeTraversal", () => ({
+  useNodeTraversal: (_, __) => ({
+    incrementBreadth,
+    incrementDepth,
+    decrementBreadth,
+    decrementDepth,
+    maxBreadth, // Should be irrelevant for these tests
+  }),
+}));
+
+describe('Hierarchy Path Templates - after traversing down, it renders path an only child', () => {
   let Tree;
   beforeAll(() => {
     Tree = renderVis(OrbitTree);
@@ -19,25 +40,19 @@ describe.skip('Hierarchy Path Templates - after traversing down, it renders path
     Tree = undefined;
   });
 
-  it('renders a path for the only child after traversing from the root', async () => {
-    const { getByTestId } = render(
+  it('by default renders no extra paths', async () => {
+    // Arrange
+    const { queryByTestId } = render(
       <MockedProvider mocks={HIERARCHY_ROOT_ONE_CHILD_MOCKS} addTypename={false}>
-          {WithCurrentSphereMockedAtom(Tree)}
-      </MockedProvider>
-    );
-
-    await waitFor(() => {
-      const traversalButton = getByTestId('traversal-button-down');
-      fireEvent.click(traversalButton);
-    });
-
-    await waitFor(() => {
-      expect(getByTestId('path-parent-one-child')).toBeTruthy();
-    });
+        {WithCurrentOrbitCoordsMockedAtom(Tree, {x: 0, y: 0})}</MockedProvider> );
+    
+    expect(queryByTestId('path-parent-one-child')).not.toBeTruthy();
+    expect(queryByTestId('path-parent-two-children-0')).not.toBeTruthy();
+    expect(queryByTestId('path-parent-two-children-1')).not.toBeTruthy();
   });
 });
 
-describe.skip('Hierarchy Path Templates - after traversing down-left, it renders a path for the first child after traversing from the root', () => {
+describe('Hierarchy Path Templates - after traversing down-left, it renders a path for the first child after traversing from the root', () => {
   let Tree;
   beforeAll(() => {
     Tree = renderVis(OrbitTree);
@@ -47,11 +62,11 @@ describe.skip('Hierarchy Path Templates - after traversing down-left, it renders
   });
   
   it('renders a path for the first child after traversing from the root', async () => {
+    // Arrange
     const { getByTestId, queryByTestId } = render(
-      <MockedProvider mocks={HIERARCHY_ROOT_TWO_CHILDREN_MOCKS} addTypename={false}>
-        {WithCurrentSphereMockedAtom(Tree)}
-      </MockedProvider>
-    );
+      <MockedProvider mocks={HIERARCHY_ROOT_ONE_CHILD_MOCKS} addTypename={false}>
+        {WithCurrentOrbitCoordsMockedAtom(Tree, {x: 0, y: 0})}</MockedProvider> );
+    
     
     await waitFor(async () => {
       const traversalButton = getByTestId('traversal-button-down-left');
