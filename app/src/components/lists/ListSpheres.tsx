c750aa6d41c6f3e3ff5e0f4882a5f28f1920c2c3
Nicholas Stebbings <nickolarse@riseup.net>
2024-04-10 17:49:41 +1200

    fix selected styles, tooltip dynamics

--- app/src/components/lists/ListSpheres.tsx
+++ app/src/components/lists/ListSpheres.tsx
@@ -57,8 +57,7 @@ function ListSpheres() {
       <PageHeader title="Spheres List" />
       <ListSortFilter label='' />
       <div className="spheres-list">
-        {sortedSpheres.map((sphere : Sphere) => <SphereCard key={sphere.id} sphere={sphere} transition={transition} isHeader={false} orbitScales={[]} runDelete={() => runDelete({variables: {id: sphere.id}})}/>)//orbits.map((orbit: Orbit) => orbit?.scale)}/>)}
-}</div>
+        {sortedSpheres.map((sphere : Sphere) => <SphereCard key={sphere.id} sphere={sphere} transition={transition} isHeader={false} orbitScales={[]} runDelete={() => runDelete({variables: {id: sphere.id}})}/>)}</div>
     </div>
   );
 }
