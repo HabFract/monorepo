b326d10077d488693d6bc4298e63fd3cb34726ea
Nicholas Stebbings <nickolarse@riseup.net>
2024-07-30 15:17:01 +0100

    add disclaimer and version

--- ui/src/BootScreen.tsx
+++ ui/src/BootScreen.tsx
@@ -6,11 +6,33 @@ import { App } from 'antd';
 import { StateMachineContext } from './contexts/state-machine';
 import { getConnection } from './graphql/connection';
 import { initGraphQLClient } from './graphql/client';
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+import { ask } from '@tauri-apps/plugin-dialog';
 
 import './App.css'
 import 'habit-fract-design-system/dist/style.css';
 import './typo.css'
 import { Spinner } from 'flowbite-react';
+
+async function checkForAppUpdates(onUserClick: false) {
+  const update = await check();
+  console.log('Checked for update :>> ', update);
+  if (!update) {
+			return;
+		} else if (update?.available) {
+      const yes = await ask(`Update to ${update.version} is available!\n\nRelease notes: ${update.body}`, { 
+        title: 'Update Available',
+        kind: 'info',
+        okLabel: 'Update',
+        cancelLabel: 'Cancel'
+      });
+      if (yes) {
+        await update.downloadAndInstall();
+        await relaunch();
+      }
+    }
+}
 
 function BootScreen({ children }: any) {
   const [connected, setConnected] = useState<boolean>(false); // Top level state machine and routing
@@ -18,6 +40,11 @@ function BootScreen({ children }: any) {
 
   useEffect(() => {
     if(connected) return;
+    checkForAppUpdates(false).then(update => {
+      console.log(update)
+    }).catch(err => {
+      console.error(err)
+    });
     getConnection().then(connection => {
       
       AppMachine.state.client = {...connection, conductorUri: connection.client.client.url!.href }
@@ -29,7 +56,7 @@ function BootScreen({ children }: any) {
     })
   }, [connected])
   
-    return !connected ? <Spinner aria-label="Loading!"size="xl" className='full-spinner' /> : (
+  return !connected ? <Spinner aria-label="Loading!"size="xl" className='full-spinner' /> : (
       <ApolloProvider client={apolloClient}>
       {/* <MyProfileProvider> */}
         <StateMachineContext.Provider value={AppMachine as any}>
--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -5,3 +5,20 @@ export const HAPP_ID = 'habit_fract'
 export const HAPP_DNA_NAME = 'habits'
 export const HAPP_ZOME_NAME_PERSONAL_HABITS = 'personal'
 export const HAPP_ZOME_NAME_PROFILES = 'profiles'
+export const ALPHA_RELEASE_DISCLAIMER = `Thank you for being an early tester of this habit tracking app.
+
+Please note that this is an alpha release. This means the software is still in the early stages of development and may not be fully featured.
+
+Notably:
+  - At this point, you cannot store habit completion across time. It is more like a hierarchical to-do list, currently!
+  - Profiles, social interaction, etc., will come later.
+  - There is no password protection, and locally cached data is not encrypted. Please don't store sensitive information if others may access the device.
+
+***
+You may encounter bugs or unexpected behavior while using the app!
+***
+
+If you experience any issues, the best course of action is to reload the app. If you want to be super helpful, you could report the bug using the form on the Habit/Fract website. Click the envelope icon on the menu for a link.
+
+We are actively working on improvements and appreciate your patience and feedback during this phase..
+`
