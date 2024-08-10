a13a0d26cee0347d879ad0747947f3e39cc0331f
Nicholas Stebbings <nickolarse@riseup.net>
2024-08-10 11:58:31 +0100

    fix vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import graphql from '@rollup/plugin-graphql';
-import svgx from "@svgx/vite-plugin-react";
 
 const host = process.env.TAURI_DEV_HOST;
+const port = process.env.VITE_UI_PORT;
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -13,7 +13,7 @@ export default ({ mode }) => {
   return defineConfig({
     server: {
       host: host || false,
-      port: 1420,
+      port: +(port as string) ||1420,
       strictPort: true,
       hmr: host
         ? {
@@ -29,7 +29,6 @@ export default ({ mode }) => {
     plugins: [react(),
     nodePolyfills(),
     graphql(),
-    svgx()
     ],
     define: {
       "process.env": {
