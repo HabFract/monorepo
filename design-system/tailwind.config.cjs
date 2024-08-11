ccf8374e9a762af11634e4f771ed84d1f06a8084
Nicholas Stebbings <nickolarse@riseup.net>
2024-08-11 11:32:18 +0100

    style tweaks

--- design-system/tailwind.config.cjs
+++ design-system/tailwind.config.cjs
@@ -64,12 +64,12 @@ export default {
 
         "title": "rgba(219,228,226, 1)", // CHINESE_WHITE
 
-        "text": "rgba(255,225,225, 1)", // WHITE
+        "text": "rgba(255,255,255, 1)", // WHITE
 
         "input-placeholder": "#4E5454",
         "input-bg": "#242424",
-        "input-text": "rgba(255,225,225, 1)",
-        "input-border": "rgba(255,225,225, 1)",
+        "input-text": "rgba(255,255,255, 1)",
+        "input-border": "rgba(255,255,255, 1)",
         
         "text-prim": "#A2EAC4;",
 
