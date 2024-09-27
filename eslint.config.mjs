cba36fb223137a704cbade36ea9dfb8af0864891
Nicholas Stebbings <nickolarse@riseup.net>
2024-09-28 10:24:57 +1200

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintPluginPrettierRecommended
+  eslintPluginPrettierRecommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+  },
 ];
