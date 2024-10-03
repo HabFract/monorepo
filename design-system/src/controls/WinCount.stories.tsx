1abfb5f54ab3bc2b070180496c26dff13aeba4fb
Nicholas Stebbings <nickolarse@riseup.net>
2024-10-04 12:30:42 +1300

    finish win component with state

--- design-system/src/controls/WinCount.stories.tsx
+++ design-system/src/controls/WinCount.stories.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { StoryFn, Meta, StoryObj } from "@storybook/react";
 import WinCount, { WinCountProps } from "./WinCount";
+import { Frequency } from "@ui/src/state";
 
 const meta: Meta<WinCountProps> = {
   title: "Components/Controls/WinCount",
   component: WinCount,
   argTypes: {},
-  render: (args: any) => (
-    <WinCount 
-    ></WinCount>
+  render: (args: WinCountProps) => (
+    <WinCount {...args} handleSaveWins={() => { console.log("Persisted wins!")}} ></WinCount>
   ),
 };
 
@@ -17,5 +17,43 @@ export default meta;
 type Story = StoryObj<WinCountProps & { content: string }>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    orbitFrequency: Frequency.DAILY_OR_MORE.DAILY,
+    currentWins: 0,
+  },
+};
+
+export const Complete: Story = {
+  args: {
+    orbitFrequency: Frequency.DAILY_OR_MORE.DAILY,
+    currentWins: 1,
+  },
+};
+
+export const TwiceDaily: Story = {
+  args: {
+    orbitFrequency: Frequency.DAILY_OR_MORE.TWO,
+    currentWins: 1
+  },
+};
+
+export const TwiceDailyComplete: Story = {
+  args: {
+    orbitFrequency: Frequency.DAILY_OR_MORE.TWO,
+    currentWins: 2
+  },
+};
+
+export const EighteenDaily: Story = {
+  args: {
+    orbitFrequency: Frequency.DAILY_OR_MORE.EIGHTEEN,
+    currentWins: 2
+  },
+};
+
+export const EighteenDailyComplete: Story = {
+  args: {
+    orbitFrequency: Frequency.DAILY_OR_MORE.EIGHTEEN,
+    currentWins: 18
+  },
 };
--- design-system/src/controls/WinCount.tsx
+++ design-system/src/controls/WinCount.tsx
@@ -1,23 +1,70 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import "./common.css";
+import { Frequency } from "@ui/src/state";
+import Progress, { ProgressProps } from "antd/es/progress";
+import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
 
 export interface WinCountProps {
-  id: string;
-  completed: boolean;
-  toggleIsCompleted: () => void;
-  size?: "main" | "secondary";
+  handleSaveWins: () => void;
+  currentWins?: number;
+  orbitFrequency?: Frequency.Rationals;
 }
 
+const twoColors: ProgressProps['strokeColor'] = {
+  '0%': 'rgba(169,189,182, 1)',
+  '100%': 'rgba(11,254,184, 1)',
+};
+
 const WinCount: React.FC<WinCountProps> = ({
-  id,
-  completed,
-  toggleIsCompleted,
-  size,
+  handleSaveWins,
+  currentWins,
+  orbitFrequency
 }) => {
+  if(!currentWins || typeof currentWins !== 'number') return console.warn("Win tracking component fed bad props");
+  const [winCount, setWinCount] = useState<number>(currentWins);
+  const [savedWinCount, setSavedWinCount] = useState<number>(currentWins);
+  const winPercent = useMemo(() => (100) * (winCount / orbitFrequency!), [winCount]);
+  const savedWinPercent = useMemo(() => (100) * (savedWinCount / orbitFrequency!), [savedWinCount]);
+  
+  const lowerLimitMet = useMemo(() => winCount <= 0, [winCount]);
+  const upperLimitMet = useMemo(() => winCount >= orbitFrequency!, [winCount]);
+  const incrementWins = () => {
+    if(upperLimitMet) return;
+    setWinCount(winCount + 1);
+  }
+  const decrementWins = () => {
+    if(lowerLimitMet) return;
+    setWinCount(winCount - 1);
+  }
+  useEffect(() => {
+    const saveToSourceChain = setInterval((() => {
+      if(savedWinPercent !== winPercent) {
+        handleSaveWins();
+        setSavedWinCount(winCount)
+      }
+      
+    }), 4000);
+  
+    return () => clearInterval(saveToSourceChain);
+  }, [savedWinPercent, winPercent, winCount, handleSaveWins]);
+
   return (
     <div className="win-count-container">
-
+      <span className="title">Wins</span>
+      { orbitFrequency! > 0
+        ? <button type="button" disabled={lowerLimitMet} className="win-control minus" onClick={decrementWins}>
+          <MinusCircleOutlined />
+        </button>
+        : <span className="spacer" />
+      }
+      <Progress percent={winPercent} format={(percent, successPercent) => <span className="win-control-indicator">{winCount}/{orbitFrequency}</span>} size={"small"} type="circle" className="text-center text-sm" strokeColor={twoColors} />
+      { orbitFrequency! > 0
+        ? <button type="button" disabled={upperLimitMet} className="win-control plus" onClick={incrementWins}>
+            <PlusCircleOutlined />
+          </button>
+        : <span className="spacer" />
+      }
     </div>
   );
 };
