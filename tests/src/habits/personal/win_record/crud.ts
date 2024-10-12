dc7c2698590f4994c79a0f712d6682dbfb379e22
Nicholas Stebbings <nickolarse@riseup.net>
2024-10-12 16:40:08 +1300

    update tests

--- tests/src/habits/personal/win_record/crud.ts
+++ tests/src/habits/personal/win_record/crud.ts
@@ -15,7 +15,7 @@ import { sortWinRecordEntryArrayByWinRecordDateIndex } from './utils';
 const { test } = pkg;
 
 export default () => {
-  test("Win Record CRUD - Happy path", async (t) => {
+  test("Win Record CRUD - Happy path 1 (single creates)", async (t) => {
     await runScenario(async (scenario) => {
       const {
         alice,
@@ -58,7 +58,7 @@ export default () => {
         const createWinRecordResponse = await callZomeAlice(
           "personal",
           "create_or_update_win_record",
-          {orbitId: encodeHashToBase64(hash2), winData: winData}
+          {orbitEh: encodeHashToBase64(hash2), winData: winData}
         );
         t.ok(createWinRecordResponse, 'a single win record was created,');
         // Then the WinRecord was created
@@ -68,59 +68,131 @@ export default () => {
         const createWinRecordResponse2 = await callZomeAlice(
           "personal",
           "create_or_update_win_record",
-          {orbitId: encodeHashToBase64(hash2), winData: winData2}
+          {orbitEh: encodeHashToBase64(hash2), winData: winData2}
         );
         t.ok(createWinRecordResponse2, 'and a multiple win record was created,');
         // Then the WinRecord was created
 
 
-        // 3. When Alice then gets all WinRecords for an orbit for a given year/month, giving the orbit hash as input 
+      } catch (e) {
+        t.ok(null);
+      }
+      await cleanup();
+    });
+  });
+  test.only("Win Record CRUD - Happy path 2  (batch creates)", async (t) => {
+    await runScenario(async (scenario) => {
+      const {
+        alice,
+        bob,
+        cleanup,
+        alice_agent_key,
+        bob_agent_key,
+        habits_cell_alice,
+        habits_cell_bob,
+      } = await setUpAliceandBob();
+
+      const callZomeAlice = async (zome_name, fn_name, payload) => {
+        return await alice.callZome({
+          cap_secret: null,
+          cell_id: habits_cell_alice,
+          zome_name,
+          fn_name,
+          payload,
+          provenance: alice_agent_key,
+        });
+      };
+      try {
+        const pauseDuration = 1000;
+        await scenario.shareAllAgents();
+        await pause(pauseDuration);
+
+        // 1. Given a Sphere has been created and we know its hash
+        // And an Orbit has been created and we know its hash
+        const hash1 = await setupSphere(callZomeAlice);
+        const createOrbitResponse = await callZomeAlice(
+          "personal",
+          "create_my_orbit",
+          anOrbit({sphereHash: encodeHashToBase64(hash1)})
+        );
+        const hash2 =  new EntryRecord<Orbit>(createOrbitResponse)?.entryHash;
+        t.ok(hash1 && hash2, "A sphere was created with an orbit linked to it,");
+
+        // When Alice then gets all WinRecords for an orbit for a given year/month, giving the orbit hash as input 
         const getOrbitWinRecordForYearMonthResponse = await callZomeAlice(
           "personal",
           "get_an_orbits_win_record_for_month",
-          {orbitId: encodeHashToBase64(hash2), yearDotMonth: "2021.01"}
+          {orbitEh: encodeHashToBase64(hash2), yearDotMonth: "2021.01"}
         );
         t.ok(getOrbitWinRecordForYearMonthResponse, 'A query was made to get win records for an orbit in a particular YYYY.MM,');
-        t.equal(getOrbitWinRecordForYearMonthResponse.length, 2, 'and the response was an array of the correct length.');
-        // Then the WinRecords were returned
+        t.equal(getOrbitWinRecordForYearMonthResponse.length, 0, 'and the response was an array of the correct length (0).');
+        // Then no WinRecords were returned
 
 
-        // 4. When Alice creates a WinRecord for two months (over a year end) with otherwise valid input, with mixed month datestrings as indexes, using the hash as an OrbitId
+        // 2. When Alice creates a WinRecord for two months (over a year end) with otherwise valid input, with mixed month datestrings as indexes, using the hash as an OrbitId
         const unbucketedWinData = {"31/12/2020": {multiple: [true, false, true]}, "02/01/2021": {multiple: [false, true, false]}};
         const createWinRecordsResponse = await callZomeAlice(
           "personal",
           "create_or_update_win_records",
-          {orbitId: encodeHashToBase64(hash2), winData: unbucketedWinData}
+          {orbitEh: encodeHashToBase64(hash2), winData: unbucketedWinData}
         );
-        t.ok(createWinRecordsResponse, 'and multiple bucketed win records were created,');
+
+        await pause(pauseDuration);
+        t.ok(createWinRecordsResponse, 'Multiple bucketed win records were created,');
         t.equal(createWinRecordsResponse?.length, 2, 'and they were returned in an array of records of the correct length,');
         const createdWinRecordEntryRecords = createWinRecordsResponse
           .map(record => new EntryRecord<WinRecord>(record))
           .sort(sortWinRecordEntryArrayByWinRecordDateIndex);
 
-        t.equal(createdWinRecordEntryRecords[0].entry.winData["31/12/2020"].multiple[0], true, 'and they returned the correct win records 1,');
-        t.equal(createdWinRecordEntryRecords[1].entry.winData["02/01/2021"].multiple[1], true, 'and they returned the correct win records 2,');
-        t.equal(createdWinRecordEntryRecords[0].entry.winData["02/01/2021"], undefined, 'and they returned a win record without the other bucketed record 1,');
-        t.equal(createdWinRecordEntryRecords[1].entry.winData["31/12/2020"], undefined, 'and they returned a win record without the other bucketed record 2,');
+        t.equal(createdWinRecordEntryRecords[0].entry.winData["31/12/2020"].multiple[0], true, 'and they returned the correct win records (1 of 2),');
+        t.equal(createdWinRecordEntryRecords[1].entry.winData["02/01/2021"].multiple[1], true, 'and they returned the correct win records (2 of 2),');
+        t.equal(createdWinRecordEntryRecords[0].entry.winData["02/01/2021"], undefined, 'and they returned a win record without the other bucketed record (1 of 2),');
+        t.equal(createdWinRecordEntryRecords[1].entry.winData["31/12/2020"], undefined, 'and they returned a win record without the other bucketed record (2 of 2),');
         // Then the WinRecords were separately created
 
-
-        // 5. When Alice then gets all WinRecords for an orbit for each created bucketed year/month, giving the orbit hash as input 
-        const getOrbitWinRecordForYearMonthResponseDec20 = await callZomeAlice(
+        // And When Alice then gets all WinRecords for an orbit for the FIRST given year/month, giving the orbit hash as input 
+        const getOrbitWinRecordForYearMonthResponse2 = await callZomeAlice(
           "personal",
           "get_an_orbits_win_record_for_month",
-          {orbitId: encodeHashToBase64(hash2), yearDotMonth: "2020.12"}
+          {orbitEh: encodeHashToBase64(hash2), yearDotMonth: "2020.12"}
         );
-        t.ok(getOrbitWinRecordForYearMonthResponseDec20, 'A query was made to get win records for an orbit in the first bucketed YYYY.MM,');
-        t.equal(getOrbitWinRecordForYearMonthResponseDec20.length, 1, 'and the response was an array of the correct length.');
-        const getOrbitWinRecordForYearMonthResponseJan21 = await callZomeAlice(
-          "personal",
-          "get_an_orbits_win_record_for_month",
-          {orbitId: encodeHashToBase64(hash2), yearDotMonth: "2021.01"}
-        );
-        t.ok(getOrbitWinRecordForYearMonthResponseJan21, 'A query was made to get win records for an orbit in the first bucketed YYYY.MM,');
-        t.equal(getOrbitWinRecordForYearMonthResponseJan21.length, 3, 'and the response was an array of the correct length (given earlier test results).');
+        const getResponse3EntryRecord0 = new EntryRecord<WinRecord>(getOrbitWinRecordForYearMonthResponse2[0]);
+        t.ok(getOrbitWinRecordForYearMonthResponse2, 'A query was made to get win records for an orbit in the first YYYY.MM,');
+        t.equal(getOrbitWinRecordForYearMonthResponse2.length, 1, 'and the response was an array of the correct length,');
+        t.deepEqual(getResponse3EntryRecord0.entry, createdWinRecordEntryRecords[0].entry, 'and the returned response included the correct WinRecord.');
         // Then the WinRecords were returned
+
+        // And When Alice then gets all WinRecords for an orbit for the SECOND given year/month, giving the orbit hash as input 
+        const getOrbitWinRecordForYearMonthResponse3 = await callZomeAlice(
+          "personal",
+          "get_an_orbits_win_record_for_month",
+          {orbitEh: encodeHashToBase64(hash2), yearDotMonth: "2021.01"}
+        );
+        const getResponse3EntryRecord1 = new EntryRecord<WinRecord>(getOrbitWinRecordForYearMonthResponse3[0]);
+        t.ok(getOrbitWinRecordForYearMonthResponse3, 'A query was made to get win records for an orbit in the second YYYY.MM,');
+        t.equal(getOrbitWinRecordForYearMonthResponse3.length, 1, 'and the response was an array of the correct length,');
+        t.deepEqual(getResponse3EntryRecord1.entry, createdWinRecordEntryRecords[1].entry, 'and the returned response included the correct WinRecord.');
+        // Then the WinRecords were returned
+
+
+
+        // 3. When Alice updates an existing linked WinRecord with otherwise valid input, giving a created WinRecord action hash as input 
+        const winDataUpdate = {orbitEh: encodeHashToBase64(hash2), winData: {"31/12/2020": {single: true}}};
+        const updateInput = {
+          winRecordId: createdWinRecordEntryRecords[0].actionHash,
+          updatedWinRecord: winDataUpdate
+        }
+        const updateWinRecordResponse = await callZomeAlice(
+          "personal",
+          "update_win_record",
+          updateInput
+        );
+        t.ok(updateWinRecordResponse, 'An update was made to a batch created WinRecord using its action hash,');
+        const updateWinRecordEntryRecord = new EntryRecord<WinRecord>(updateWinRecordResponse);
+        t.ok(updateWinRecordEntryRecord, 'and the response was a singular WinRecord record,');
+        t.deepEqual(updateWinRecordEntryRecord.entry, winDataUpdate, 'with the updated details.');
+        // Then the updated WinRecords was returned
+
       } catch (e) {
         t.ok(null);
       }
--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,5 +1,6 @@
 import {orbits1, orbits2, crud, levels, prepend} from './habits/personal/orbits';
 import {crud as sphereCrud} from './habits/personal/sphere';
+import {crud as winRecordCrud} from './habits/personal/win_record';
 
 orbits1();
 orbits2();
@@ -8,3 +9,4 @@ levels();
 
 sphereCrud();
 prepend();
+winRecordCrud();
