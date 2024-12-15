190871faa715f1588031daba9200717e23f8ad73
Nicholas Stebbings <nickolarse@riseup.net>
2024-12-15 17:01:46 +1300

    click on calendar to move to day

--- design-system/src/components/controls/CalendarDay.tsx
+++ design-system/src/components/controls/CalendarDay.tsx
@@ -6,11 +6,13 @@ import { DateTime } from "luxon";
 export interface CalendarDayProps {
   dateString: string;
   completed: boolean;
+  onClickDay: (date: DateTime) => void;
 }
 
 const CalendarDay: React.FC<CalendarDayProps> = ({
   dateString,
   completed,
+  onClickDay
 }) => {
   const dateFromIso = DateTime.fromISO(dateString);
   const day = dateFromIso.day;
@@ -20,7 +22,7 @@ const CalendarDay: React.FC<CalendarDayProps> = ({
       <div className="calendar-day-title">
         { weekDay }
       </div>
-      <div className={completed ? "calendar-day-complete" : "calendar-day-incomplete"}>
+      <div onClick={() => onClickDay(dateFromIso)} className={completed ? "calendar-day-complete" : "calendar-day-incomplete"}>
         { day }
       </div>
     </div>
